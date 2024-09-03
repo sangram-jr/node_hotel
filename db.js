7c8f887bfb9e540b51e5f6b100d594747dca7a7d
sangram-jr <sangrambera58@gmail.com>
2024-09-04 00:19:27 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose=require('mongoose');
 
 //define the MongoDB url
-const mongoURL='mongodb://localhost:27017/hotels' //replace mydatabase with your database name
+//const mongoURL='mongodb://localhost:27017/hotels' //replace mydatabase with your database name//it is local database
+//const mongoURL='mongodb+srv://sangrambera58:sangrm58@cluster0.fuphyfm.mongodb.net/hotels'//its a mongodb atlas database
+require('dotenv').config();
+const mongoURL=process.env.MONGODB_URL;
+//const mongoURL=process.enc.MONGODB_URL_LOCAL;
 
 //set up mongodb connection
 mongoose.connect(mongoURL)
